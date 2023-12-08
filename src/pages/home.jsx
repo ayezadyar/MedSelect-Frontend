c338c2525892b6c51e1f480ab67a856a9a758039
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-08 16:13:56 +0500

    side nav

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,13 +1,48 @@
-import React from 'react';
-import Login from './login';
-import Header from '../components/header';
-import { Link } from "react-router-dom"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
+	const [isNavOpen, setNavOpen] = useState(false);
+
+	const toggleNav = () => {
+		setNavOpen(!isNavOpen);
+	};
+
 	return (
-		<div>
-			{/* <Header/> */}
-			<div className="flex flex-col justify-center items-center min-h-screen">
+		<div className="flex">
+			{/* Side Navigation */}
+			<div
+				className={`fixed top-0 left-0 h-full bg-teal-500 w-64 text-white transform ${isNavOpen ? 'translate-x-0' : '-translate-x-full'
+					} transition-transform duration-300 ease-in-out overflow-y-auto`}
+			>
+				<div className="p-4">
+					<Link to="/" className="block py-2" onClick={toggleNav}>
+						HOME
+					</Link>
+					<Link to="/about" className="block py-2" onClick={toggleNav}>
+						ABOUT US
+					</Link>
+					<Link to="/contact" className="block py-2" onClick={toggleNav}>
+						CONTACT US
+					</Link>
+					<Link to="/how-it-works" className="block py-2" onClick={toggleNav}>
+						HOW IT WORKS
+					</Link>
+				</div>
+			</div>
+
+			{/* Main Content */}
+			<div className="flex flex-col flex-1 justify-center items-center min-h-screen">
+				{/* Burger Icon */}
+				<div
+					className={`absolute top-4 left-4 cursor-pointer ${isNavOpen ? 'text-white' : 'text-black'
+						} font-bold`}
+					onClick={toggleNav}
+				>
+					<div className="w-6 h-px bg-current mb-1"></div>
+					<div className="w-6 h-px bg-current mb-1"></div>
+					<div className="w-6 h-px bg-current"></div>
+				</div>
 
 				{/* Logo or Picture */}
 				<div className="mb-4 mt-2">
