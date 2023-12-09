c9b65225c02a2fa6c0221e64cb43ad83c0b98595
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-09 19:05:34 +0500

    move card to component

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faHippo } from "@fortawesome/free-solid-svg-icons";
+import HomeCard from '../components/homeCard';
 
 export default function Home() {
 	const [isNavOpen, setNavOpen] = useState(false);
@@ -95,67 +96,31 @@ export default function Home() {
 
 					</div>
 				</form>
-				{/* Cards */}
+				{/* HomeCards */}
 				<div className="flex flex-col sm:flex-row flex-wrap justify-around w-full max-w-6xl mb-8">
+					{/* HomeCard 1 */}
+					<HomeCard
+						imageSrc="/doseAlarm.png"
+						altText="Dose Alarm"
+						title="Dose Alarm"
+						to="/login"
+					/>
 
-					{/* Card 1 */}
-					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
-							<Link to="/login">
-								<div className="flex flex-col items-center">
-									<img
-										src="/doseAlarm.png"
-										alt="Dose Alarm"
-										className="w-28 h-28 mx-auto my-10 mb-2 object-cover rounded"
-									/>
-									<h2 className="text-base sm:text-lg font-semibold mb-2">Dose Alarm</h2>
-								</div>
-							</Link >
-						</div>
-						<div className="hidden absolute top-full left-0 w-full bg-white p-4 rounded shadow-md opacity-0 transition-opacity duration-300">
+					{/* HomeCard 2 */}
+					<HomeCard
+						imageSrc="/doctorConsultation.png"
+						altText="Doctor Consultation"
+						title="Doctor Consultation"
+						to="/login"
+					/>
 
-						</div>
-					</div>
-
-					{/* Card 2 */}
-					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
-							<Link to="/login">
-								<div className="flex flex-col items-center">
-									<img
-										src="/doctorConsultation.png"
-										alt="Doctor Consultation"
-										className="w-28 h-28 mx-auto my-10 mb-2 object-cover rounded"
-									/>
-									<h2 className="text-base sm:text-lg font-semibold mb-2">Doctor Consultation</h2>
-								</div>
-							</Link>
-						</div>
-						<div className="hidden absolute top-full left-0 w-full bg-white p-4 rounded shadow-md opacity-0 transition-opacity duration-300">
-
-						</div>
-					</div>
-
-					{/* Card 3 */}
-					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
-							<Link to="/login">
-
-								<div className="flex flex-col items-center">
-									<img
-										src="/community.png"
-										alt="Community"
-										className="w-28 h-28 mx-auto my-10 mb-2 object-cover rounded"
-									/>
-									<h2 className="text-base sm:text-lg font-semibold mb-2">Community</h2>
-								</div>
-							</Link>
-
-						</div>
-						<div className="hidden absolute top-full left-0 w-full bg-white p-4 rounded shadow-md opacity-0 transition-opacity duration-300">
-						</div>
-					</div>
-
+					{/* HomeCard 3 */}
+					<HomeCard
+						imageSrc="/community.png"
+						altText="Community"
+						title="Community"
+						to="/login"
+					/>
 				</div>
 			</div>
 		</div>
