c0798072f5cdb2faaa1ed23b06235c825e601faf
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-09 15:08:30 +0500

    change hover color

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
 				<div className="p-4">
 					<Link
 						to="/"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
+						className="block py-2 hover:bg-[#294a26] transition-all duration-300"
 						onClick={toggleNav}
 						style={{ marginTop: isNavOpen ? '22px' : '0' }}
 					>
@@ -26,28 +26,28 @@ export default function Home() {
 					</Link>
 					<Link
 						to="/about"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
+						className="block py-2 hover:bg-[#294a26] transition-all duration-300"
 						onClick={toggleNav}
 					>
 						ABOUT US
 					</Link>
 					<Link
 						to="/community"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
+						className="block py-2 hover:bg-[#294a26] transition-all duration-300"
 						onClick={toggleNav}
 					>
 						JOIN COMMUNITY
 					</Link>
 					<Link
 						to="/contact"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
+						className="block py-2 hover:bg-[#294a26] transition-all duration-300"
 						onClick={toggleNav}
 					>
 						CONTACT US
 					</Link>
 					<Link
 						to="/how-it-works"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
+						className="block py-2 hover:bg-[#294a26] transition-all duration-300"
 						onClick={toggleNav}
 					>
 						HOW IT WORKS
@@ -83,7 +83,7 @@ export default function Home() {
 							aria-label="Medicine search"
 						/>
 						<button
-							className="flex-shrink-0 bg-[#517028] hover:bg-teal-700 text-white font-bold py-2 px-4 rounded hover:scale-110"
+							className="flex-shrink-0 bg-[#517028] hover:bg-[#294a26] text-white font-bold py-2 px-4 rounded hover:scale-110"
 							type="submit"
 						>
 							Search
@@ -95,7 +95,7 @@ export default function Home() {
 
 					{/* Card 1 */}
 					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-teal-700 transition-all duration-300 hover:scale-110">
+						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
 							<Link to="/login">
 								<div className="flex flex-col items-center">
 									<img
@@ -114,7 +114,7 @@ export default function Home() {
 
 					{/* Card 2 */}
 					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-teal-700 transition-all duration-300 hover:scale-110">
+						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
 							<Link to="/login">
 								<div className="flex flex-col items-center">
 									<img
@@ -133,7 +133,7 @@ export default function Home() {
 
 					{/* Card 3 */}
 					<div className="w-full mb-4 relative overflow-hidden rounded-lg sm:w-1/2 lg:w-1/3 xl:w-1/4">
-						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-teal-700 transition-all duration-300 hover:scale-110">
+						<div className="bg-[#517028] shadow-md rounded p-4 h-64 text-white cursor-pointer hover:bg-[#294a26] transition-all duration-300 hover:scale-110">
 							<Link to="/login">
 
 								<div className="flex flex-col items-center">
