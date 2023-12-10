17460b44fae0c3c3bec2599f312b3b8c1f3becf8
ayezadyar <ayezadmirza469@gmail.com>
2023-12-10 13:10:09 +0500

    update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHippo, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import HomeCard from '../components/homeCard';
+import SideNav from '../components/sideNav';
+import Papa from 'papaparse';
 
 export default function About() {
 	const [isNavOpen, setNavOpen] = useState(false);
@@ -11,64 +16,21 @@ export default function About() {
 	return (
 		<div className="flex">
 			{/* Side Navigation */}
-			<div
-				className={`fixed top-0 left-0 h-full bg-teal-500 w-64 text-white transform ${isNavOpen ? 'translate-x-0' : '-translate-x-full'
-					} transition-transform duration-300 ease-in-out overflow-y-auto`}
-			>
-				<div className="p-4">
-					<Link
-						to="/"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
-						onClick={toggleNav}
-						style={{ marginTop: isNavOpen ? '22px' : '0' }}
-					>
-						HOME
-					</Link>
-					<Link
-						to="/about"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
-						onClick={toggleNav}
-					>
-						ABOUT US
-					</Link>
-					<Link
-						to="/community"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
-						onClick={toggleNav}
-					>
-						JOIN COMMUNITY
-					</Link>
-					<Link
-						to="/contact"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
-						onClick={toggleNav}
-					>
-						CONTACT US
-					</Link>
-					<Link
-						to="/how-it-works"
-						className="block py-2 hover:bg-teal-700 transition-all duration-300"
-						onClick={toggleNav}
-					>
-						HOW IT WORKS
-					</Link>
-				</div>
-			</div>
+			<SideNav isNavOpen={isNavOpen} toggleNav={toggleNav} />
 
 			{/* Main Content */}
-			<div className="flex flex-col flex-1 justify-center items-center min-h-screen">
+			<div
+				className={`flex flex-col flex-1 justify-center items-center min-h-screen transition-margin duration-300 ${isNavOpen ? 'ml-64' : ''}`}
+			>
 				{/* Burger Icon */}
-				<div
-					className={`absolute top-4 left-4 cursor-pointer ${isNavOpen ? 'text-white' : 'text-black'
-						} font-bold`}
+				<button
+					className={`absolute top-4 left-4 cursor-pointer font-bold ${isNavOpen ? 'text-white' : 'text-black'}`}
 					onClick={toggleNav}
 				>
-					<div className="w-6 h-px bg-current mb-1"></div>
-					<div className="w-6 h-px bg-current mb-1"></div>
-					<div className="w-6 h-px bg-current"></div>
-				</div>
+					<FontAwesomeIcon icon={faBars} size="lg" />
+				</button>
+				
 			</div>
 		</div>
-
 	);
 }
