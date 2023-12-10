728a133fe46ff60697d82277a8c12280508a1381
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-10 22:22:38 +0500

    multiple dose alarm skeleton

--- src/pages/doseAlarm.jsx
+++ src/pages/doseAlarm.jsx
@@ -1,68 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import SideNav from '../components/sideNav'; // Import your SideNav component
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faBell } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faBell, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Howl } from 'howler';
 
 const AlarmClock = () => {
+	const [alarms, setAlarms] = useState([{ name: '', time: '', isActive: false }]);
 	const [currentTime, setCurrentTime] = useState('');
-	const [alarmTime, setAlarmTime] = useState('');
-	const [isAlarmActive, setIsAlarmActive] = useState(false);
 	const [isNavOpen, setNavOpen] = useState(false); // State for side nav
 
 	const sound = new Howl({
 		src: ['/alarm.mp3'],
 	});
 
-	const playSound = () => {
-		sound.play();
-	};
-
 	useEffect(() => {
 		const interval = setInterval(() => {
 			const newTime = new Date().toLocaleTimeString('en-US', { hour12: true });
 			setCurrentTime(newTime);
 
-			if (formatTime(newTime) === alarmTime && isAlarmActive) {
-				playSound()
-				alert('Alarm ringing!');
-				// You can replace this alert with any sound or other notification
-				handleAlarmOff();
-			}
+			alarms.forEach((alarm, index) => {
+				if (formatTime(newTime) === alarm.time && alarm.isActive) {
+					sound.play();
+					alert(`Alarm for ${alarm.name} ringing!`);
+					handleAlarmOff(index);
+				}
+			});
 		}, 1000);
 
 		return () => clearInterval(interval);
-	}, [alarmTime, isAlarmActive]);
+	}, [alarms]);
 
-	// Helper function to convert 12-hour format to 24-hour format for comparison
 	const formatTime = (time12h) => {
 		const [time, modifier] = time12h.split(' ');
 		let [hours, minutes] = time.split(':');
 		if (hours === '12') {
 			hours = '00';
 		}
-		if (modifier === 'PM') {
+		if (modifier === 'PM' && hours !== '12') {
 			hours = parseInt(hours, 10) + 12;
 		}
 		return `${hours}:${minutes}:00`;
 	};
 
-	const handleAlarmChange = (event) => {
-		const timeValue = formatTime(event.target.value + ' AM'); // Default to AM for input; adjust as needed
-		setAlarmTime(timeValue);
-		setIsAlarmActive(true);
+	const handleAlarmChange = (index, event) => {
+		const updatedAlarms = [...alarms];
+		updatedAlarms[index].time = formatTime(event.target.value + ' AM'); // Adjust as needed
+		updatedAlarms[index].isActive = true;
+		setAlarms(updatedAlarms);
 	};
 
-	const handleAlarmOff = () => {
-		setIsAlarmActive(false);
-		setAlarmTime('');
+	const handleNameChange = (index, event) => {
+		const updatedAlarms = [...alarms];
+		updatedAlarms[index].name = event.target.value;
+		setAlarms(updatedAlarms);
+	};
+
+	const handleAlarmOff = (index) => {
+		const updatedAlarms = [...alarms];
+		updatedAlarms[index].isActive = false;
+		updatedAlarms[index].time = '';
+		setAlarms(updatedAlarms);
+	};
+
+	const addAlarm = () => {
+		setAlarms([...alarms, { name: '', time: '', isActive: false }]);
 	};
 
 	const toggleNav = () => {
 		setNavOpen(!isNavOpen);
 	};
-
-
 
 	return (
 		<div className="flex flex-col h-screen">
@@ -81,30 +87,42 @@ const AlarmClock = () => {
 					Alarm Clock <FontAwesomeIcon icon={faBell} />
 				</h1>
 
-				<div className="mb-4 w-80 md:w-96 text-center">
-					<div className="bg-[#517028] text-white p-4 rounded-lg shadow-md">
-						<h2 className="text-lg md:text-2xl mb-2 md:mb-4">Current Time: {currentTime}</h2>
-						<input
-							type="time"
-							onChange={handleAlarmChange}
-							className="border border-[#294a26] text-[#294a26] font-bold text-lg md:text-2xl p-3 md:p-4 rounded focus:outline-none"
-						/>
-						<p className="mt-2 text-md md:text-lg">
-							Set Alarm for:{' '}
-							<span className="text-white text-md md:text-lg font-medium">
-								{alarmTime ? new Date('1970-01-01T' + alarmTime).toLocaleTimeString('en-US', { hour12: true }) : ''}
-							</span>
-						</p>
-						{isAlarmActive && (
-							<button
-								onClick={handleAlarmOff}
-								className="mt-4 bg-[#294a26] text-md md:text-lg font-medium text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none"
-							>
-								Turn Alarm Off
-							</button>
-						)}
+				{alarms.map((alarm, index) => (
+					<div key={index} className="mb-4 w-80 md:w-96 text-center">
+						<div className="bg-[#517028] text-white p-4 rounded-lg shadow-md">
+							<input
+								type="text"
+								placeholder="Enter Dose Name"
+								value={alarm.name}
+								onChange={(e) => handleNameChange(index, e)}
+								className="border border-[#294a26] text-[#294a26] font-bold text-lg md:text-2xl p-3 md:p-4 rounded focus:outline-none mb-2"
+							/>
+							<input
+								type="time"
+								value={alarm.time.slice(0, -3)}
+								onChange={(e) => handleAlarmChange(index, e)}
+								className="border border-[#294a26] text-[#294a26] font-bold text-lg md:text-2xl p-3 md:p-4 rounded focus:outline-none"
+							/>
+							<p className="mt-2 text-md md:text-lg">
+								Set Alarm for: <span className="text-white text-md md:text-lg font-medium">
+									{alarm.time ? new Date('1970-01-01T' + alarm.time).toLocaleTimeString('en-US', { hour12: true }) : ''}
+								</span>
+							</p>
+							{alarm.isActive && (
+								<button
+									onClick={() => handleAlarmOff(index)}
+									className="mt-4 bg-[#294a26] text-md md:text-lg font-medium text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none"
+								>
+									Turn Alarm Off
+								</button>
+							)}
+						</div>
 					</div>
-				</div>
+				))}
+
+				<button onClick={addAlarm} className="mt-4 bg-[#294a26] text-md md:text-lg font-medium text-white px-4 py-2 rounded hover:bg-green-600 focus:outline-none">
+					<FontAwesomeIcon icon={faPlus} /> Add Another Alarm
+				</button>
 			</div>
 		</div>
 	);
