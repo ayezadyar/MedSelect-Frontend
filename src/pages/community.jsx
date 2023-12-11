fa178484ab2876352c6c751891b842f573f6c31d
ayezadyar <ayezadmirza469@gmail.com>
2023-12-11 17:57:17 +0500

    community form- side nav

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -1,12 +1,22 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import HomeCard from "../components/homeCard";
+import SideNav from "../components/sideNav";
+import Papa from "papaparse";
 
-const community = () => {
+const Community = () => {
+  const [isNavOpen, setNavOpen] = useState(false);
   const [feedback, setFeedback] = useState('');
   const [image, setImage] = useState(null);
   const [allFeedback, setAllFeedback] = useState([]);
   const [includeImage, setIncludeImage] = useState(false);
   const [searchKeyword, setSearchKeyword] = useState('');
   const scrollTargetRef = useRef(null);
+
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
 
   const handleInputChange = (e) => {
     setFeedback(e.target.value);
@@ -57,87 +67,101 @@ const community = () => {
   };
 
   return (
-    <div className="container mx-auto my-8">
-      <h1 className="text-2xl font-bold mb-4">Discussion Form</h1>
+    <div className="flex">
+      {/* Side Navigation */}
+      <SideNav isNavOpen={isNavOpen} toggleNav={toggleNav} />
 
-      <div className="relative mb-8">
-        <label className="block mb-2">Search Keyword:</label>
-        <input
-          type="text"
-          value={searchKeyword}
-          onChange={handleSearchChange}
-          placeholder="Enter keyword to highlight"
-          className="border rounded p-2 w-48 sm:w-64 absolute top-0 right-0 mt-2 mr-2"
-        />
-      </div>
-
-      <form onSubmit={handleSubmit} className="mb-8">
-        <label className="block mb-2">Your Feedback:</label>
-        <textarea
-          rows="4"
-          cols="50"
-          value={feedback}
-          onChange={handleInputChange}
-          className="border rounded p-2 w-full"
-          placeholder="Share your feedback..."
-        ></textarea>
-
-        <div className="mt-4 mb-4">
-          <button
-            type="button"
-            onClick={handleToggleImage}
-            className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
-              includeImage ? 'bg-opacity-50' : ''
-            }`}
-          >
-            {includeImage ? 'Remove Image' : 'Add Image'}
-          </button>
-        </div>
-
-        {includeImage && (
-          <div>
-            <label className="block mt-2 mb-2">Upload Image:</label>
-            <input
-              type="file"
-              accept="image/*"
-              onChange={handleImageChange}
-              className="mb-4"
-            />
-          </div>
-        )}
-
-        <button
-          type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Submit Feedback
+      {/* Main Content */}
+      <div className={`flex flex-col flex-1 justify-center items-center min-h-screen transition-margin duration-300 ${isNavOpen ? "ml-64" : ""}`}>
+        {/* Burger Icon */}
+        <button className={`absolute top-4 left-4 cursor-pointer font-bold ${isNavOpen ? "text-white" : "text-black"}`} onClick={toggleNav}>
+          <FontAwesomeIcon icon={faBars} size="lg" />
         </button>
-      </form>
 
-      <div ref={scrollTargetRef}>
-        <h2 className="text-xl font-bold mb-4">All Feedback:</h2>
-        {allFeedback.length === 0 ? (
-          <p>No feedback yet. Be the first to share!</p>
-        ) : (
-          <ul>
-            {allFeedback.map((item, index) => (
-              <li key={index} className="mb-4">
-                <p>{highlightText(item.text)}</p>
-                {item.image && (
-                  <img
-                    src={item.image}
-                    alt={`User Feedback ${index}`}
-                    className="mt-2 rounded"
-                    style={{ maxWidth: '100%', maxHeight: '200px' }}
-                  />
-                )}
-              </li>
-            ))}
-          </ul>
-        )}
+        {/* Your Feedback Form */}
+        <div className="container mx-auto my-8">
+        <h1 className="text-2xl font-bold mb-4">Discussion Form</h1>
+
+<div className="relative mb-8">
+  <label className="block mb-2">Search Keyword:</label>
+  <input
+    type="text"
+    value={searchKeyword}
+    onChange={handleSearchChange}
+    placeholder="Enter keyword to highlight"
+    className="border rounded p-2 w-48 sm:w-64 absolute top-0 right-0 mt-2 mr-2"
+  />
+</div>
+
+<form onSubmit={handleSubmit} className="mb-8">
+  <label className="block mb-2">Your Feedback:</label>
+  <textarea
+    rows="4"
+    cols="50"
+    value={feedback}
+    onChange={handleInputChange}
+    className="border rounded p-2 w-full"
+    placeholder="Share your feedback..."
+  ></textarea>
+
+  <div className="mt-4 mb-4">
+    <button
+      type="button"
+      onClick={handleToggleImage}
+      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
+        includeImage ? 'bg-opacity-50' : ''
+      }`}
+    >
+      {includeImage ? 'Remove Image' : 'Add Image'}
+    </button>
+  </div>
+
+  {includeImage && (
+    <div>
+      <label className="block mt-2 mb-2">Upload Image:</label>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageChange}
+        className="mb-4"
+      />
+    </div>
+  )}
+
+  <button
+    type="submit"
+    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+  >
+    Submit Feedback
+  </button>
+</form>
+
+<div ref={scrollTargetRef}>
+  <h2 className="text-xl font-bold mb-4">All Feedback:</h2>
+  {allFeedback.length === 0 ? (
+    <p>No feedback yet. Be the first to share!</p>
+  ) : (
+    <ul>
+      {allFeedback.map((item, index) => (
+        <li key={index} className="mb-4">
+          <p>{highlightText(item.text)}</p>
+          {item.image && (
+            <img
+              src={item.image}
+              alt={`User Feedback ${index}`}
+              className="mt-2 rounded"
+              style={{ maxWidth: '100%', maxHeight: '200px' }}
+            />
+          )}
+        </li>
+      ))}
+    </ul>
+  )}
+</div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default community;
+export default Community;
