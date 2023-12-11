e7836a00a19fe8412953e330c6b789cbf023f84d
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-11 18:59:55 +0500

    button color

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -80,84 +80,83 @@ const Community = () => {
 
         {/* Your Feedback Form */}
         <div className="container mx-auto my-8">
-        <h1 className="text-2xl font-bold mb-4">Discussion Form</h1>
+          <h1 className="text-2xl font-bold mb-4">Discussion Form</h1>
 
-<div className="relative mb-8">
-  <label className="block mb-2">Search Keyword:</label>
-  <input
-    type="text"
-    value={searchKeyword}
-    onChange={handleSearchChange}
-    placeholder="Enter keyword to highlight"
-    className="border rounded p-2 w-48 sm:w-64 absolute top-0 right-0 mt-2 mr-2"
-  />
-</div>
-
-<form onSubmit={handleSubmit} className="mb-8">
-  <label className="block mb-2">Your Feedback:</label>
-  <textarea
-    rows="4"
-    cols="50"
-    value={feedback}
-    onChange={handleInputChange}
-    className="border rounded p-2 w-full"
-    placeholder="Share your feedback..."
-  ></textarea>
-
-  <div className="mt-4 mb-4">
-    <button
-      type="button"
-      onClick={handleToggleImage}
-      className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${
-        includeImage ? 'bg-opacity-50' : ''
-      }`}
-    >
-      {includeImage ? 'Remove Image' : 'Add Image'}
-    </button>
-  </div>
-
-  {includeImage && (
-    <div>
-      <label className="block mt-2 mb-2">Upload Image:</label>
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageChange}
-        className="mb-4"
-      />
-    </div>
-  )}
-
-  <button
-    type="submit"
-    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-  >
-    Submit Feedback
-  </button>
-</form>
-
-<div ref={scrollTargetRef}>
-  <h2 className="text-xl font-bold mb-4">All Feedback:</h2>
-  {allFeedback.length === 0 ? (
-    <p>No feedback yet. Be the first to share!</p>
-  ) : (
-    <ul>
-      {allFeedback.map((item, index) => (
-        <li key={index} className="mb-4">
-          <p>{highlightText(item.text)}</p>
-          {item.image && (
-            <img
-              src={item.image}
-              alt={`User Feedback ${index}`}
-              className="mt-2 rounded"
-              style={{ maxWidth: '100%', maxHeight: '200px' }}
+          <div className="relative mb-8">
+            <label className="block mb-2">Search Keyword:</label>
+            <input
+              type="text"
+              value={searchKeyword}
+              onChange={handleSearchChange}
+              placeholder="Enter keyword to highlight"
+              className="border rounded p-2 w-48 sm:w-64 absolute top-0 right-0 mt-2 mr-2"
             />
-          )}
-        </li>
-      ))}
-    </ul>
-  )}
-</div>
+          </div>
+
+          <form onSubmit={handleSubmit} className="mb-8">
+            <label className="block mb-2">Your Feedback:</label>
+            <textarea
+              rows="4"
+              cols="50"
+              value={feedback}
+              onChange={handleInputChange}
+              className="border rounded p-2 w-full"
+              placeholder="Share your feedback..."
+            ></textarea>
+
+            <div className="mt-4 mb-4">
+              <button
+                type="button"
+                onClick={handleToggleImage}
+                className={`bg-[#517028] hover:bg-[#294a26] text-white font-bold py-2 px-4 rounded ${includeImage ? 'bg-opacity-50' : ''
+                  }`}
+              >
+                {includeImage ? 'Remove Image' : 'Add Image'}
+              </button>
+            </div>
+
+            {includeImage && (
+              <div>
+                <label className="block mt-2 mb-2">Upload Image:</label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                  className="mb-4"
+                />
+              </div>
+            )}
+
+            <button
+              type="submit"
+              className="bg-[#517028] hover:bg-[#294a26] text-white font-bold py-2 px-4 rounded"
+            >
+              Submit Feedback
+            </button>
+          </form>
+
+          <div ref={scrollTargetRef}>
+            <h2 className="text-xl font-bold mb-4">All Feedback:</h2>
+            {allFeedback.length === 0 ? (
+              <p>No feedback yet. Be the first to share!</p>
+            ) : (
+              <ul>
+                {allFeedback.map((item, index) => (
+                  <li key={index} className="mb-4">
+                    <p>{highlightText(item.text)}</p>
+                    {item.image && (
+                      <img
+                        src={item.image}
+                        alt={`User Feedback ${index}`}
+                        className="mt-2 rounded"
+                        style={{ maxWidth: '100%', maxHeight: '200px' }}
+                      />
+                    )}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
