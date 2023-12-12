78de204bef4e337fa247428aa24bb0018a7a7710
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-13 02:09:06 +0500

    contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,169 +1,80 @@
+
 import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./contactStyle.css";
 import SideNav from "../components/sideNav";
 
-const Contact = () => {
+const contact = () => {
   const [isNavOpen, setNavOpen] = useState(false);
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    emailAddress: "",
-    subject: "",
-    message: "",
-  });
-  const firebaseConfig = {
-    apiKey: "AIzaSyAvMA1Wryh0Sbq3T6SlMlpfHinwFzBl-BA",
-    authDomain: "medselect-e1aec.firebaseapp.com",
-    projectId: "medselect-e1aec",
-    storageBucket: "medselect-e1aec.appspot.com",
-    messagingSenderId: "271973218747",
-    appId: "1:271973218747:web:48fb260edbf68285a9ac9e",
-  };
-
-  const app = initializeApp(firebaseConfig);
-  const db = getDatabase(app);
 
   const toggleNav = () => {
     setNavOpen(!isNavOpen);
   };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Initialize Firebase
-
-    try {
-      // Add form data to Realtime Database
-      const dbRef = ref(db, "contacts");
-      const newContactRef = ref(
-        db,
-        `contacts/${formData.firstName}-${formData.lastName}`
-      );
-      await set(newContactRef, formData);
-
-      console.log("Document written with ID: ", newContactRef.key);
-
-      // Reset form after submission
-      setFormData({
-        firstName: "",
-        lastName: "",
-        emailAddress: "",
-        subject: "",
-        message: "",
-      });
-      alert("Contact Info stored successfully!");
-    } 
-    catch (e) {
-      console.error("Error adding document: ", e);
-      alert("Error storing data. Please try again.");
-    }
-  };
-
   return (
     <div className="flex">
+      {/* Side Navigation */}
       <SideNav isNavOpen={isNavOpen} toggleNav={toggleNav} />
+
       {/* Main Content */}
       <div
-        className={`flex flex-col flex-1 justify-center items-center min-h-screen transition-margin duration-300 ${isNavOpen ? "ml-64" : ""
-          }`}
+        className={`flex flex-col justify-center items-center min-h-screen transition-margin duration-300 ${isNavOpen ? "ml-64" : ""}`}
       >
+        {/* Burger Icon */}
         <button
-          className={`absolute top-4 left-4 cursor-pointer font-bold ${isNavOpen ? "text-white" : "text-black"
-            }`}
+          className={`absolute top-4 left-4 cursor-pointer font-bold ${isNavOpen ? "text-white" : "text-black"}`}
           onClick={toggleNav}
         >
           <FontAwesomeIcon icon={faBars} size="lg" />
         </button>
 
-        {/* Your Form */}
-        <div className="container">
-          <div className="text">Contact us</div>
-          <form onSubmit={handleSubmit}>
-            <div className="form-row">
-              <div className="input-data">
-                <input
-                  type="text"
-                  name="firstName"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  required
-                />
-                <div className="underline"></div>
-                <label htmlFor="">First Name</label>
-              </div>
-              <div className="input-data">
-                <input
-                  type="text"
-                  name="lastName"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  required
-                />
-                <div className="underline"></div>
-                <label htmlFor="">Last Name</label>
-              </div>
+      </div>
+
+      <div className="container">
+        <div className="text">Contact us</div>
+        <form action="#">
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline"></div>
+              <label htmlFor="">First Name</label>
             </div>
-            <div className="form-row">
-              <div className="input-data">
-                <input
-                  type="text"
-                  name="emailAddress"
-                  value={formData.emailAddress}
-                  onChange={handleChange}
-                  required
-                />
-                <div className="underline"></div>
-                <label htmlFor="">Email Address</label>
-              </div>
-              <div className="input-data">
-                <input
-                  type="text"
-                  name="subject"
-                  value={formData.subject}
-                  onChange={handleChange}
-                  required
-                />
-                <div className="underline"></div>
-                <label htmlFor="">Subject</label>
-              </div>
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline"></div>
+              <label htmlFor="">Last Name</label>
             </div>
-            <div className="form-row">
-              <div className="input-data textarea">
-                <textarea
-                  rows="8"
-                  cols="80"
-                  name="message" // Add this line
-                  value={formData.message}
-                  onChange={handleChange}
-                  required
-                ></textarea>
-                <br />
-                <div className="underline"></div>
-                <label htmlFor="message">Write your message</label>
-              </div>
+          </div>
+          <div className="form-row">
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline"></div>
+              <label htmlFor="">Email Address</label>
             </div>
-            {/* Submit Button */}
-            <div className="form-row submit-btn">
-              <div className="input-data">
-                <div className="inner"></div>
-                <input type="submit" value="Submit" />
-              </div>
+            <div className="input-data">
+              <input type="text" required />
+              <div className="underline"></div>
+              <label htmlFor="">Subject</label>
             </div>
-          </form>
-        </div>
+          </div>
+          <div className="form-row">
+            <div className="input-data textarea">
+              <textarea rows="8" cols="80" required></textarea>
+              <br />
+              <div className="underline"></div>
+              <label htmlFor="">Write your message</label>
+            </div>
+          </div>
+          <center><div className="form-row bg-[#517028] hover:bg-[#294a26] text-white  overflow: hidden w-44 rounded-md cursor-pointer">
+            <div className="input-data">
+              <div className="inner"></div>
+              <input type="submit" value="submit" />
+            </div>
+          </div></center>
+        </form>
       </div>
     </div>
   );
 };
 
-export default Contact;
+export default contact;
