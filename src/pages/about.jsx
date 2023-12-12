3ca7546d106151ce9ceee4f53291195bd947f850
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-12 19:50:35 +0500

    about_fast

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -37,9 +37,9 @@ export default function About() {
 					<div className="w-full md:w-1/2 p-0 md:p-8 flex items-center justify-center ">
 
 						<img
-							src="logo.png"
+							src="fast.png"
 							alt="About Us"
-							className="w-96 h-auto object-cover rounded-tr-full rounded-bl-ful"
+							className="w-full h-auto object-cover  rounded-bl-ful"
 						/>
 					</div>
 
@@ -50,23 +50,37 @@ export default function About() {
 								About Us
 							</h2>
 							<p className="text-lg md:text-xl text-white mb-8">
-								Welcome to my premier real estate agency, where our mission is to provide exceptional service and expertise to help our clients achieve their real estate goals. With years of experience in the industry, our team of dedicated professionals has built a reputation for excellence and integrity.
-								<br />
-								<br />
-								We are committed to staying up-to-date with the latest trends and technologies in the real estate market, and we use our knowledge and expertise to guide our clients through the buying and selling process with confidence.
+								Medselect is an innovative healthcare platform providing informed decision-making tools. Services include identifying alternative medicines, locating nearby pharmacies, and offering expert consultations. The vibrant community forum encourages shared experiences and insights. Medselect empowers users with comprehensive information for confident healthcare navigation. It enhances individuals' ability to make informed and effective medical treatment decisions.
 							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			{/* Introduction Section */}
+			
+			<div id="about" className="flex flex-col md:flex-row w-full h-screen ">
 
-			<div className="bg-[#294a26] text-white p-8 rounded-xl shadow-md">
-				<h2 className="text-3xl font-bold mb-4 text-center text-white">Introduction</h2>
-				<p className="text-lg mb-8 leading-loose">
-					Medselect is a meticulously designed and comprehensive platform that serves as an invaluable resource for individuals seeking essential information about medications. It offers a range of essential services to empower users in their healthcare decision-making journey. Firstly, it provides Informative Insights, ensuring that users have access to valuable information about alternative or substitute medicines, helping them make well-informed choices regarding their treatment options. Secondly, Medselect offers Effortless Locating by swiftly identifying nearby pharmacies that stock the required medication, simplifying the often cumbersome procurement process. Additionally, Medselect stands out by providing opportunities for Expert Consultations, allowing users to engage with verified specialists and experienced pharmacists who offer personalized guidance on specific medicines. Furthermore, the platform fosters a Vibrant Community forum where users can engage in discussions, sharing their experiences, and discussing the advantages and disadvantages of particular medications. This sense of community support facilitates informed decisions and enhances the overall healthcare experience for users.
-				</p>
+				
+
+				<div className="w-full md:w-1/2 p-4 md:p-8 bg-[#517028] flex items-center justify-center">
+					<div className="text-center md:text-left">
+						<h2 className="text-3xl md:text-5xl font-bold text-white mb-4">
+							Introduction
+						</h2>
+						<p className="text-lg md:text-xl text-white mb-8">
+							Medselect is a comprehensive platform offering Informative Insights on medications, aiding users in making informed treatment choices. It provides Effortless Locating of nearby pharmacies for streamlined procurement. Expert Consultations connect users with verified specialists for personalized guidance. The Vibrant Community forum fosters discussions and shared experiences for informed decisions. Overall, Medselect enhances the healthcare journey by empowering users with essential information and community support.
+						</p>
+					</div>
+				</div>
+				<div className="w-full md:w-1/2 p-0 md:p-8 flex items-center justify-center ">
+
+					<img
+						src="logo.png"
+						alt="About Us"
+						className="w-96 h-auto object-cover  rounded-bl-ful"
+					/>
+				</div>
 			</div>
+
 		</>
 	);
 }
