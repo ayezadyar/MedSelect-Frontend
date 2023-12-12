ac531073192306eedccdde3e821d138baa698e15
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2023-12-12 19:05:00 +0500

    comunity code

--- src/pages/community.jsx
+++ src/pages/community.jsx
@@ -1,23 +1,23 @@
 import { FaPaperclip } from 'react-icons/fa';
+import './CommunitySection.css'; // Import the CSS file for styling
 import React, { useEffect, useState } from 'react';
-import './CommunitySection.css'; // Import the CSS file
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faHippo, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import HomeCard from '../components/homeCard';
 import SideNav from '../components/sideNav';
+import Papa from 'papaparse';
 
-const Community = () => {
+const community = () => {
   const [isNavOpen, setNavOpen] = useState(false);
-  const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState('');
-  const [replyText, setReplyText] = useState('');
-  const [image, setImage] = useState(null);
-  const [externalComments, setExternalComments] = useState([]);
-  const [showPastComments, setShowPastComments] = useState(false);
-  const [pastComments, setPastComments] = useState([]);
 
   const toggleNav = () => {
     setNavOpen(!isNavOpen);
   };
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState('');
+  const [replyText, setReplyText] = useState('');
+  const [image, setImage] = useState(null);
 
   const handleCommentChange = (event) => {
     setNewComment(event.target.value);
@@ -53,40 +53,18 @@ const Community = () => {
     }
   };
 
-  useEffect(() => {
-    // Example API endpoint for fetching external comments
-    const apiEndpoint = 'https://api.example.com/comments';
-
-    fetch(apiEndpoint)
-      .then((response) => response.json())
-      .then((data) => setExternalComments(data))
-      .catch((error) => console.error('Error fetching comments:', error));
-
-    // Set example past comments
-    setPastComments([
-      { text: 'Past Comment 1', additionalInfo: 'Additional Info 1' },
-      { text: 'Past Comment 2', additionalInfo: 'Additional Info 2' },
-    ]);
-  }, []);
-
-  const handleGoBack = () => {
-    window.history.back();
-  };
-
   return (
     <div className="flex">
       {/* Side Navigation */}
       <SideNav isNavOpen={isNavOpen} toggleNav={toggleNav} />
       <div
-        className={`flex flex-col flex-1 justify-center items-center min-h-screen transition-margin duration-300 ${
-          isNavOpen ? "ml-64" : ""
-        }`}
+        className={`flex flex-col flex-1 justify-center items-center min-h-screen transition-margin duration-300 ${isNavOpen ? "ml-64" : ""
+          }`}
       >
         {/* Burger Icon */}
         <button
-          className={`absolute top-4 left-4 cursor-pointer font-bold ${
-            isNavOpen ? "text-white" : "text-black"
-          }`}
+          className={`absolute top-4 left-4 cursor-pointer font-bold ${isNavOpen ? "text-white" : "text-black"
+            }`}
           onClick={toggleNav}
         >
           <FontAwesomeIcon icon={faBars} size="lg" />
@@ -94,25 +72,6 @@ const Community = () => {
       </div>
       <div className="comment-section">
         <h2>Comment Section</h2>
-        <button onClick={() => setShowPastComments(!showPastComments)} className="toggle-past-comments-btn">
-          {showPastComments ? 'Hide Past Comments' : 'Show Past Comments'}
-        </button>
-        {showPastComments && (
-          <div className="past-comments-section">
-            {/* Render past comments here */}
-            {/* You can use a similar structure as you did for user-submitted and external comments */}
-            <ul className="past-comments-list">
-              {/* Map over past comments and render them */}
-              {pastComments.map((comment, index) => (
-                <li key={index} className="past-comment-item">
-                  <p>{comment.text}</p>
-                  <p>{comment.additionalInfo}</p>
-                  {/* Include any additional information for past comments */}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
         <form onSubmit={handleCommentSubmit} className="comment-form">
           <input
             type="text"
@@ -136,25 +95,52 @@ const Community = () => {
           </button>
         </form>
         <ul className="comment-list">
-          {/* User-submitted comments */}
           {comments.slice().reverse().map((comment, index) => (
             <li key={index} className="comment-item">
-              {/* ... existing user-submitted comment code */}
-            </li>
-          ))}
-          {/* External comments */}
-          {externalComments.map((externalComment, index) => (
-            <li key={`external-${index}`} className="comment-item external-comment">
-              {/* ... existing external comment code */}
+              <p>{comment.text}</p>
+              {comment.image && (
+                <img src={URL.createObjectURL(comment.image)} alt="Comment Attachment" className="comment-image" />
+              )}
+              <ul className="reply-list">
+                {comment.replies.map((reply, replyIndex) => (
+                  <li key={replyIndex} className="reply-item">
+                    <p>{reply.text}</p>
+                    {reply.image && (
+                      <img src={URL.createObjectURL(reply.image)} alt="Reply Attachment" className="reply-image" />
+                    )}
+                  </li>
+                ))}
+                <li>
+                  <form onSubmit={handleReplySubmit(index)} className="reply-form">
+                    <input
+                      type="text"
+                      value={replyText}
+                      onChange={handleReplyChange}
+                      placeholder="Reply to this comment"
+                      className="reply-input"
+                    />
+                    <label htmlFor={`reply-image-${index}`} className="attachment-icon">
+                      <FaPaperclip />
+                      <input
+                        type="file"
+                        id={`reply-image-${index}`}
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        style={{ display: 'none' }}
+                      />
+                    </label>
+                    <button type="submit" className="reply-btn">
+                      Submit Reply
+                    </button>
+                  </form>
+                </li>
+              </ul>
             </li>
           ))}
         </ul>
-        <button onClick={handleGoBack} className="go-back-btn">
-          Go Back
-        </button>
       </div>
     </div>
   );
 };
 
-export default Community;
+export default community;
