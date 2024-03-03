5f4f0b16a871360ac64aac59a22470b4ba596f7d
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-03 23:12:44 +0500

    remove himePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from "./pages/home"
 import About from "./pages/about"
 import Contact from './pages/contact'
@@ -7,24 +7,21 @@ import Login from './pages/login'
 import Signup from './pages/Signup'
 import Community from './pages/community'
 import DoseAlarm from './pages/doseAlarm'
-import HomePage from './pages/homePage'
 
 function App() {
- 
+
   return (
     <>
-    <Routes>
-      <Route path="/" element= { < Home /> } />
-      <Route path="/home" element= { < HomePage /> } />
-      <Route path="/about" element= { <About /> } />
-      <Route path="/contact" element= { <Contact/> } />
-      <Route path="/community" element= { <Community/> } />
-      <Route path="/login" element= { <Login/> } />
-      <Route path="/signup" element = {<Signup/>}/>      
-      <Route path="/doseAlarm" element= { <DoseAlarm/> } />
-    </Routes>
+      <Routes>
+        <Route path="/" element={< Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/doseAlarm" element={<DoseAlarm />} />
+      </Routes>
     </>
   )
 }
 export default App
- 
