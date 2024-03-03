5d75af5a1ed538cc7b6f36c410d4a82f0fde86c5
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-03 23:38:32 +0500

    overlay issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    hmr: {
+      overlay: false
+    }
+  }
 })
