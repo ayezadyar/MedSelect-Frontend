d8dcd81025b2580f772fd1b50d5bf2709026f068
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-20 01:59:41 +0500

    Update chat.jsx

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -3,7 +3,7 @@ import { query, collection, orderBy, onSnapshot, limit } from "firebase/firestor
 import { db } from "../Firebase";
 import Message from "./Message";
 import SendMessage from "./sendMessage";
-
+import { FiSend, FiPaperclip, FiFileText } from 'react-icons/fi';
 const ChatBox = () => {
 	const [messages, setMessages] = useState([]);
 	const scroll = useRef();
@@ -43,7 +43,14 @@ const ChatBox = () => {
 							<Message message={message} />
 							{message.fileUrl && (
 								<a href={message.fileUrl} target="_blank" rel="noopener noreferrer" className="inline-block mt-2">
-									<img src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md border" />
+									{message.fileUrl.includes('.pdf') ? (
+										// Show a default PDF icon if the URL includes ".pdf"
+										// <img src="/pdf.svg" alt="PDF" className="rounded-lg shadow-md border h-20 w-20 object-cover" />
+										<FiFileText className="h-10 w-10 ml-10" />
+									) : (
+										// Otherwise, attempt to display the image
+										<img src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md border" />
+									)}
 								</a>
 							)}
 						</div>
