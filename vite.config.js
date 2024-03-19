b62da2620f1bf741e83b34b772481d02b898dfc2
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-19 21:36:33 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,21 @@
-// vite.config.js
+// // vite.config.js
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     hmr: {
+//       overlay: false
+//     }
+//   }
+// })
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    hmr: {
-      overlay: false
-    }
-  }
 })
