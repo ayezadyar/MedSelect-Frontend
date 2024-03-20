7eb305e3c0b0e97731d4f47dffd2164a2cc89e30
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-21 00:58:48 +0500

    Update messageDoc.jsx

--- src/components/doctors/messageDoc.jsx
+++ src/components/doctors/messageDoc.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const MessageDoc = ({ message }) => {
 	// Determine message alignment and styling based on the sender
-	const messageAlignmentClass = message.isSender ? 'justify-end' : 'justify-start';
-	const messageBgColorClass = message.isSender ? 'bg-blue-500' : 'bg-gray-300';
+	const messageAlignmentClass = message.isSender ? 'items-end' : 'items-start';
+	const messageBgColorClass = message.isSender ? 'bg-[#517028]' : 'bg-gray-300';
 	const textColorClass = message.isSender ? 'text-white' : 'text-black';
 
 	return (
-		<div className={`flex ${messageAlignmentClass} my-2 mx-4`}>
+		<div className={`flex flex-col ${messageAlignmentClass} my-2 mx-4`}>
 			{/* Text Message */}
 			{message.message && (
 				<div className={`message-text ${messageBgColorClass} ${textColorClass} p-4 rounded-lg max-w-xs md:max-w-md lg:max-w-lg`}>
@@ -17,9 +17,7 @@ const MessageDoc = ({ message }) => {
 
 			{/* Image File */}
 			{message.imageUrl && (
-				<div className={`${messageAlignmentClass} max-w-xs md:max-w-md lg:max-w-lg`}>
-					<img src={message.imageUrl} alt="Uploaded File" className="rounded-lg mt-2" style={{ maxWidth: '100%', height: 'auto' }} />
-				</div>
+				<img src={message.imageUrl} alt="Uploaded File" className="rounded-lg mt-2" style={{ maxWidth: '50%', maxHeight: '200px', height: 'auto' }} />
 			)}
 		</div>
 	);
