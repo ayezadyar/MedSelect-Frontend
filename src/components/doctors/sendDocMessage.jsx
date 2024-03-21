99a379313769e0ef8460daea3d2f9293223f3497
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-21 14:14:02 +0500

    clip icon

--- src/components/doctors/sendDocMessage.jsx
+++ src/components/doctors/sendDocMessage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
-const SendDOCMessage = ({ onSend,scroll }) => {
+import { FiPaperclip } from 'react-icons/fi';
+const SendDOCMessage = ({ onSend, scroll }) => {
 	const [message, setMessage] = useState('');
 	const [file, setFile] = useState(null); // State to manage the uploaded file
 
@@ -19,8 +19,20 @@ const SendDOCMessage = ({ onSend,scroll }) => {
 		setFile(e.target.files[0]); // Set the file to the first file if multiple files were selected
 	};
 
+
+
+
 	return (
 		<form onSubmit={handleSubmit} className="send-message-form bg-gray-100 p-4 fixed bottom-0 w-full flex justify-between items-center">
+			<label htmlFor="file-upload" className="file-upload-label mr-2">
+				<FiPaperclip size={24} className="file-icon" />
+				<input
+					id="file-upload"
+					type="file"
+					onChange={handleFileChange}
+					className="hidden"
+				/>
+			</label>
 			<input
 				type="text"
 				value={message}
@@ -28,16 +40,12 @@ const SendDOCMessage = ({ onSend,scroll }) => {
 				placeholder="Type a message..."
 				className="input-field border rounded-full py-2 px-4 flex-grow mr-2"
 			/>
-			<input
-				type="file"
-				onChange={handleFileChange}
-				className="file-input mr-2"
-			/>
 			<button type="submit" className="send-button bg-[#517028] hover:bg-[#294a26] text-white font-bold py-2 px-4 rounded-full">
 				Send
 			</button>
 		</form>
 	);
+
 };
 
 export default SendDOCMessage;
