550d8ed82f79484e02ab0491931bba53f6392423
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-23 01:53:39 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -18,8 +18,8 @@ if ('serviceWorker' in navigator) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App />
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 );
