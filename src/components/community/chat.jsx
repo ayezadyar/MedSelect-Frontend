2926d192ed13339d9e22b9eae4d2f9a119212f90
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-03-23 16:32:02 +0500

    Update chat.jsx

--- src/components/community/chat.jsx
+++ src/components/community/chat.jsx
@@ -7,6 +7,7 @@ import { FiFileText } from 'react-icons/fi';
 import SideNav from "../sideNav";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { format, isToday, isSameDay } from 'date-fns'; // Import functions for date formatting and comparison
 
 const ChatBox = () => {
 	const [messages, setMessages] = useState([]); // State to hold messages
@@ -17,23 +18,28 @@ const ChatBox = () => {
 
 	useEffect(() => {
 		const q = query(
-			collection(db, "messages"), // Collection reference from Firebase
-			orderBy("createdAt", "desc"), // Order messages by createdAt in descending order
-			limit(50) // Limit messages to 50
+			collection(db, "messages"),
+			orderBy("createdAt", "desc"),
+			limit(50)
 		);
 
 		const unsubscribe = onSnapshot(q, (querySnapshot) => {
-			const fetchedMessages = []; // Array to hold fetched messages
+			const fetchedMessages = [];
 			querySnapshot.forEach((doc) => {
-				fetchedMessages.push({ ...doc.data(), id: doc.id }); // Push each message into the array
+				const data = doc.data();
+				// Ensure createdAt is an object with a 'seconds' property
+				if (data.createdAt && typeof data.createdAt.seconds === 'number') {
+					fetchedMessages.push({ ...data, id: doc.id });
+				}
 			});
-			const sortedMessages = fetchedMessages.sort(
-				(a, b) => a.createdAt.seconds - b.createdAt.seconds
-			); // Sort messages by createdAt
-			setMessages(sortedMessages); // Set messages state
+
+			const sortedMessages = fetchedMessages.sort((a, b) => a.createdAt.seconds - b.createdAt.seconds);
+			setMessages(sortedMessages);
 		});
-		return () => unsubscribe(); // Cleanup function to unsubscribe from Firebase
+
+		return () => unsubscribe();
 	}, []);
+
 
 	useEffect(() => {
 		if (scroll.current) {
@@ -51,58 +57,86 @@ const ChatBox = () => {
 		const baseName = parts.slice(0, -1).join('.');
 		return baseName;
 	}
+	
+	const toDate = (timestamp) => {
+		return timestamp ? new Date(timestamp.seconds * 1000) : new Date();
+	};
+	function formatDate(timestamp) {
+		const date = timestamp.toDate();
+		return date.toLocaleDateString();
+	}
+
+	// Transform messages into a grouped structure
+	const groupMessagesByDate = (messages) => {
+		return messages.reduce((acc, message) => {
+			// Convert Firestore timestamp to Date object
+			const date = toDate(message.createdAt).toDateString();
+			if (!acc[date]) {
+				acc[date] = [];
+			}
+			acc[date].push(message);
+			return acc;
+		}, {});
+	};
+
+	const messagesGroupedByDate = groupMessagesByDate(messages);
+	// Now, when rendering, iterate over `groupedMessages` to display each group
 
 	return (
 		<div className="flex h-screen">
-			{/* SideNav Component */}
 			<SideNav isNavOpen={isNavOpen} toggleNav={toggleNav} />
-
-			{/* Burger Icon for toggling SideNav */}
 			<button
 				className={`absolute top-4 left-4 z-50 cursor-pointer font-bold ${isNavOpen ? 'text-white' : 'text-black'}`}
 				onClick={toggleNav}
 			>
 				<FontAwesomeIcon icon={faBars} size="lg" />
 			</button>
-
-			{/* Main Chat Content, adjusting padding based on SideNav state */}
 			<main className={`flex flex-col flex-grow transition-padding duration-500 ${isNavOpen ? 'pl-64' : 'pl-0'}`}>
-				{/* Messages List */}
 				<div className="flex-grow overflow-auto p-4">
-					<div className="messages-wrapper space-y-4 flex flex-col items-end">
-						{messages.map((message) => (
-							<div key={message.id}
-								className="break-words max-w-xs p-4 mb-4 rounded shadow-md border border-gray-300 bg-white">
-								<Message message={message} /> {/* Message Component */}
-								{message.fileUrl && (
-									<a href={message.fileUrl} target="_blank" rel="noopener noreferrer" className="inline-block mt-2">
-										{message.fileUrl.includes('.pdf') ? (
-											<div className="flex items-center">
-												<FiFileText className="h-10 w-10 text-gray-700" />
-												<div className="ml-2">
-													{getFileNameFromUrl(decodeURIComponent(getFileNameWithoutExtension(message.fileUrl)))}
-												</div>
-											</div>
-										) : message.fileUrl.includes('.mp4') || message.fileUrl.includes('.webm') || message.fileUrl.includes('.mkv') ? (
-											<video src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md " autoPlay />
-										) : (
-											<img src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md " />
-										)}
-									</a>
-								)}
+					{Object.keys(messagesGroupedByDate).map((date) => (
+						<div key={date}>
+							{/* Header for the date */}
+							<div className="text-center my-4 relative">
+								<hr className="border-t border-gray-200" />
+								<span className="absolute left-1/2 top-1/2 bg-white px-2 -translate-x-1/2 -translate-y-1/2 transform">
+									{isToday(new Date(date)) ? 'Today' : format(new Date(date), 'MMMM dd, yyyy')}
+								</span>
 							</div>
-						))}
-					</div>
+
+
+							{/* Messages for the date */}
+							<div className="space-y-4 flex flex-col items-end">
+								{messagesGroupedByDate[date].map((message) => (
+									<div key={message.id} className="break-words max-w-xs p-4 mb-4 rounded shadow-md border border-gray-300 bg-white">
+										<Message message={message} />
+										{message.fileUrl && (
+											<a href={message.fileUrl} target="_blank" rel="noopener noreferrer" className="inline-block mt-2">
+												{message.fileUrl.includes('.pdf') ? (
+													<div className="flex items-center">
+														<FiFileText className="h-10 w-10 text-gray-700" />
+														<div className="ml-2">
+															{getFileNameFromUrl(decodeURIComponent(getFileNameWithoutExtension(message.fileUrl)))}
+														</div>
+													</div>
+												) : message.fileUrl.includes('.mp4') || message.fileUrl.includes('.webm') || message.fileUrl.includes('.mkv') ? (
+													<video src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md" autoPlay muted loop />
+												) : (
+													<img src={message.fileUrl} alt="Uploaded File" className="rounded-lg shadow-md" />
+												)}
+											</a>
+										)}
+									</div>
+								))}
+							</div>
+						</div>
+					))}
 					<span ref={scroll}></span>
 				</div>
-				{/* Send Message Component */}
-				<div className={`p-4 bg-gray-50 border-t border-gray-300`}>
+				<div className="p-4 bg-gray-50 border-t border-gray-300">
 					<SendMessage scroll={scroll} />
 				</div>
 			</main>
 		</div>
 	);
-
-};
-
+}
 export default ChatBox;
